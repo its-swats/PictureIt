d66cd202f22b9f5fca11931f465976d680eac7da
Shawn Watson <shawnw86@gmail.com>
2016-05-07 13:15:58 -0700

    add database migrations

---
+++ app/assets/javascripts/pic_submission.js
@@ -0,0 +1,26 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+
+
+$(document).ready(function(){
+	var takePicture = $('#upload-file');
+		takePicture.change(function(event) {
+	});
+	submitPhotoHandler();
+	
+});
+
+
+function submitPhotoHandler(){
+	$('#photoSubmission').submit(function(event){
+		event.preventDefault();
+		caption = $("input[name='caption'").val();
+		photo = $('#upload-file')[0].files[0];
+		var response = $.ajax({
+			method: 'POST',
+			url: '/',
+			data: {'caption': caption, 'photo': photo}
+		});
+	})
+};
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,8 @@ class MainController < ApplicationController
 	def index
 
 	end
+
+	def create
+		p params
+	end
 end
