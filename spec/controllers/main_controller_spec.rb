aad2e9d52baa76d4c78d278f29db0946b572bce8
Shawn Watson <shawnw86@gmail.com>
2016-05-15 11:44:28 -0700

    Add tests for keep & kill routes, further tests for main#create

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe MainController, type: :controller do
 		include CarrierWave::Test::Matchers
 		let(:test_photo){FactoryGirl.create(:photo)}
 		let(:uploader){ImageUploader.new(test_photo, :image)}
+		let(:upvote){FactoryGirl.create(:keep)}
 
 		it 'creates a new photo entry and changes count by 1 when an image is provided' do
 			expect{post :create, photo: test_photo.attributes}.to change{Photo.count}.by(1)
@@ -17,6 +18,24 @@ RSpec.describe MainController, type: :controller do
 
 		it 'does not require a caption to create an entry' do
 			expect {post :create, photo: {image: 'fake image'}}.to change{Photo.count}.by(1)
+		end
+
+		it 'clears all votes when a new image is uploaded' do
+			upvote.save
+			post :create, photo: {image: 'test image'}
+			expect(Vote.count).to eq(0)
+		end
+	end
+
+	describe '#kill' do
+		it 'creates a new downvote when accessed' do
+			expect {post :kill}.to change{Vote.count}.by(1)
+		end
+	end
+
+	describe '#keep' do
+		it 'creates a new upvote when accessed' do
+			expect {post :keep}.to change{Vote.count}.by(1)
 		end
 	end
 end
--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :vote do
-    
+		factory :keep do
+			vote true
+		end
+		factory :kill do
+			vote false
+		end    
   end
 end
